74646793b9447703b2d148dc8524fb5293417c03
Nahuel Zapata <inahuelzapata@gmail.com>
2018-09-30 20:54:31 -0300

    Initial Commit

---
+++ Swifty-IoC/Swifty_IoC.h
@@ -0,0 +1,19 @@
+//
+//  Swifty_IoC.h
+//  Swifty-IoC
+//
+//  Created by Nahuel Zapata on 9/30/18.
+//  Copyright © 2018 iNahuelZapata. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Swifty_IoC.
+FOUNDATION_EXPORT double Swifty_IoCVersionNumber;
+
+//! Project version string for Swifty_IoC.
+FOUNDATION_EXPORT const unsigned char Swifty_IoCVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Swifty_IoC/PublicHeader.h>
+
+
